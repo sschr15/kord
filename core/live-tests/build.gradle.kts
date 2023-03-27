143b2268f4be28e817c06f715dcf2afb46be1ea7
Michael Rittmeister <michael@rittmeister.in>
2023-03-27 09:24:39 +0200

    Fix dokkaHtmlMultiModule task

--- core/live-tests/build.gradle.kts
+++ core/live-tests/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.dokka.gradle.DokkaTaskPartial
+
 plugins {
     `kord-internal-multiplatform-module`
 }
@@ -19,5 +21,10 @@ kotlin {
 tasks {
     withType<AbstractTestTask>().configureEach {
         enabled = !System.getenv("KORD_TEST_TOKEN").isNullOrBlank()
+    }
+
+    // Replace this once proper project isolation support is there
+    task<DokkaTaskPartial>("dokkaHtmlPartial") {
+        enabled = false
     }
 }
