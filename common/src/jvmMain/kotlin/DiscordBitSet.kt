913427ccc6f53b8f8d34bf83d98e6097765f275c
Lukellmann <lukellmann@gmail.com>
2023-03-30 19:35:47 +0200

    Rename files

--- common/src/jvmMain/kotlin/DiscordBitSet.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("DiscordBitSetJvm")
-package dev.kord.common
-
-import java.math.BigInteger
-
-internal actual fun formatIntegerFromBigEndianByteArray(data: ByteArray): String = BigInteger(data).toString()
-internal actual fun parseIntegerToBigEndianByteArray(value: String): ByteArray = BigInteger(value).toByteArray()
