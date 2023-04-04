f98b117f5c729d129f68e3c7fe9957d831dde4b5
Lukellmann <lukellmann@gmail.com>
2023-04-04 23:24:33 +0200

    Rename nonJvm source set to nonJvmMain

--- common/src/nonJvm/kotlin/ConcurrentHashMap.kt
+++
@@ -1,7 +0,0 @@
-package dev.kord.common
-
-import co.touchlab.stately.collections.ConcurrentMutableMap
-import dev.kord.common.annotation.KordInternal
-
-@KordInternal
-public actual typealias ConcurrentHashMap<K, V> = ConcurrentMutableMap<K, V>
